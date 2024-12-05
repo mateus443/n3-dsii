f98b433c17abb505502df91d4ae6c0b3b184904f
mateus443 <mateusp2110@gmail.com>
2024-12-05 20:14:21 -0300

    Delete Aula8 directory

--- Aula8/database.js
+++
@@ -1,9 +0,0 @@
-const { Sequelize } = require('sequelize');
-
-// Conexão com o banco de dados SQLite
-const sequelize = new Sequelize({
-    dialect: 'sqlite',       // Especifica o SQLite como o banco de dados
-    storage: './database.sqlite'  // Local onde o banco de dados será salvo
-});
-
-module.exports = sequelize;
